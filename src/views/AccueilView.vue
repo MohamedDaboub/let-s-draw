b964b8585a88a8cbfa378babc8fdc9e71e4fbec5
Flo714 <florentin.demortiere@edu.univ-fcomte.fr>
2022-05-19 17:53:55 +0200

    HTML - Page d'accueil sans les médias

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -1,3 +1,85 @@
 <template>
-    <h1>let's draw</h1>
+    <header>
+        <div>
+            <div>
+                <!-- Logo Let's Draw-->
+            </div>
+            <nav>
+                <a href="">Tendances</a>
+                <a href="">Glossaire</a>
+                <a href="">Nous Contacter</a>
+                <div>
+                    <!-- Logo don -->
+                    <a href="">Faire un don</a>
+                </div>
+                <div>
+                    <!-- logo s'inscrire -->
+                    <a href="">S'inscrire</a>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <body>
+        <div>
+            <h1>Enfants & Artiste, présentez votre coup de pinceau, partagez votre talent sur Let’s Draw</h1>
+
+            <!-- Image début -->
+            <img src="" alt="">
+        </div>
+
+        <div>
+           <a href="">Envoyer mon dessin</a> 
+           <a href="">Faire un dessin</a> 
+        </div>
+        
+        <h2>Les Dernieres Tendances</h2>
+
+        <div>
+            <div>
+                <img src="" alt="">
+
+                <div>
+                    <p>Jean Vart</p>
+                    <p>Peintre</p>
+                </div>
+
+                <div>
+                    <p>7 Jours</p>
+                </div>
+                <div>
+                    <!-- Étoiles -->
+                </div>
+            </div>
+        </div>
+    </body>
+    <footer>
+        
+        <div>
+            <h3>Lien</h3>
+            <a href="">Faire un don</a>
+            <a href="">Tendances</a>
+            <a href="">Nous contacter</a>
+            <a href="">Glossaire</a>
+        </div>
+         <div>
+            <h3>Obtenir de l'aide</h3>
+            <p>Lets.Draw250@gmail.com</p>
+            <a href="">À propos</a>
+        </div>
+        <div>
+            <h3>Information</h3>
+            <a href="">Mentions Légales</a>
+        </div>
+        <div>
+            <h3>Suivez-nous</h3>
+            <!-- Logo Réseaux Sociaux -->
+        </div>
+        <div>
+            <!-- Image Paiement -->
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+        </div>
+    </footer>
 </template>
