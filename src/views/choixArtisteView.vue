0c05ae18e1008695fbbd4ca04bc251e9013c4dc2
Flo714 <florentin.demortiere@edu.univ-fcomte.fr>
2022-05-19 21:10:32 +0200

    HTML - Page choix artiste sans les médias

--- src/views/choixArtisteView.vue
+++ src/views/choixArtisteView.vue
@@ -1,3 +1,96 @@
 <template>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium ratione eius amet reiciendis, repellendus laudantium fugit cupiditate vero unde incidunt minima nobis odio explicabo quo sapiente molestiae alias, deleniti natus?</p>
+    <header>
+        <div>
+            <div>
+                <!-- Logo Let's Draw-->
+            </div>
+            <nav>
+                <a href="">Tendances</a>
+                <a href="">Glossaire</a>
+                <a href="">Nous Contacter</a>
+                <div>
+                    <!-- Logo don -->
+                    <a href="">Faire un don</a>
+                </div>
+                <div>
+                    <!-- logo s'inscrire -->
+                    <a href="">S'inscrire</a>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <body>
+        <div>
+            <h1>Choix de l’artiste</h1>
+
+            <p>Pour envoyer votre dessin veullez choisir un artiste en fonction de son style et de vos préférences</p>
+        </div>
+
+        <div>
+            <p>Trier par :</p>
+
+            <div>
+                <p>Tous</p>
+
+                <!-- Logo Fleche bas -->
+
+            </div>
+
+            <div>
+                <p>Les mieux notés</p>
+
+                <!-- Logo Fleche bas -->
+            </div>
+
+            <div>
+                <p>De la semaine</p>
+
+                <!-- Logo Fleche bas -->
+            </div>
+        </div>
+
+        <div>
+            <img src="" alt="">
+
+            <p>Florent Vacot</p>
+            <p>Peintre</p>
+
+            <!-- Logos étoiles -->
+        </div>
+
+        <div>
+            <!--Passez à la page suivante (voir maquette)-->
+        </div>
+
+    </body>
+    <footer>
+        
+        <div>
+            <h3>Lien</h3>
+            <a href="">Faire un don</a>
+            <a href="">Tendances</a>
+            <a href="">Nous contacter</a>
+            <a href="">Glossaire</a>
+        </div>
+         <div>
+            <h3>Obtenir de l'aide</h3>
+            <p>Lets.Draw250@gmail.com</p>
+            <a href="">À propos</a>
+        </div>
+        <div>
+            <h3>Information</h3>
+            <a href="">Mentions Légales</a>
+        </div>
+        <div>
+            <h3>Suivez-nous</h3>
+            <!-- Logo Réseaux Sociaux -->
+        </div>
+        <div>
+            <!-- Image Paiement -->
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+        </div>
+    </footer>
 </template>
