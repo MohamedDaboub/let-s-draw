c2707d5f3063cec01a800f832ea9b19f85f8e3a6
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-05-22 21:11:23 +0200

    card

--- src/views/AproposView.vue
+++ src/views/AproposView.vue
@@ -56,15 +56,15 @@
     <div class="max-w-sm rounded overflow-hidden shadow-lg bg-gradient-to-t from-purple-500 to-pink-500">
         <img class="w-full" src="../../public/img-squirrel/Mohamed-daboub.webp" alt="une image de Mohamed Daboub qui fait le Développement dans le groupe">
         <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+            <div class="font-bold text-xl mb-2">Mohamed Daboub</div>
             <p class="text-gray-700 text-base">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
             </p>
         </div>
         <div class="px-6 pt-4 pb-2">
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">                                <li><a href="https://www.linkedin.com/in/mohamed-daboub-12376a224/"><i class="bi bi-linkedin"></i></a></li></span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">                                <li><a href="https://www.linkedin.com/in/mohamed-daboub-12376a224/"><i class="bi bi-linkedin"></i></a></li></span>
+            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">                                <li><a href="https://www.linkedin.com/in/mohamed-daboub-12376a224/"><i class="bi bi-linkedin"></i></a></li></span>
         </div>
         </div>
     </body>
