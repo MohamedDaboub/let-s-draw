72c6a1a8f03885fa703c61cef4af2f2f7611f401
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-05-24 22:23:58 +0200

    a propos presque fini

--- src/views/AproposView.vue
+++ src/views/AproposView.vue
@@ -1,20 +1,47 @@
 <template>
     <body class="text-white">
         <h1 class=" text-3xl p-12">Qui sommes-nous ?</h1>
-        <p class=" text-lg px-16 py-8 ">Nous sommes 3 étudiants en BUT MMI. Nous avons réaliser ce concept lors d’un projet durant notre 1 ère année.  Merci de votre compréhension.</p>
-        <div class="flex gap-10 justify-center py-10">
+        <p class=" text-xl px-16 py-8 ">Nous sommes 3 étudiants en BUT MMI. Nous avons réaliser ce concept lors d’un projet durant notre 1 ère année.  Merci de votre compréhension.</p>
+        <div class="flex gap-10 justify-center py-10 flex-wrap">
+            <div class="max-w-sm rounded overflow-hidden shadow-lg bg-gradient-to-t from-batman to-batman1 ">
+                <img class="w-full" src="../../public/img-squirrel/Florenten_Demortiere.webp" alt="une image de Mohamed Daboub qui fait le Développement dans le groupe">
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">Florentin Demortiere</div>
+                    <p class="text-white text-base">
+                    Co-Créateur de let's draw, Designer.
+                    </p>
+                </div>
+                <div class="px-6 pt-4 pb-2 flex justify-center gap-7">
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2 mb-2  hover:animate-[spin_2000ms_1]  "><a href="https://www.pinterest.fr/flodemortiere9154/_saved/"><i class="bi bi-pinterest"></i></a></span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2 mb-2  hover:animate-[spin_2000ms_1] "><a href="https://www.instagram.com/flo.dem_/"><i class="bi bi-instagram"></i></a></span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2 mb-2  hover:animate-[spin_2000ms_1] "> <a href="https://www.linkedin.com/in/florentin-demortiere-a19551224/"><i class="bi bi-linkedin "></i></a></span>
+                </div>
+            </div>
             <div class="max-w-sm rounded overflow-hidden shadow-lg bg-gradient-to-t from-batman to-batman1 ">
                 <img class="w-full" src="../../public/img-squirrel/Mohamed-daboub.webp" alt="une image de Mohamed Daboub qui fait le Développement dans le groupe">
                 <div class="px-6 py-4">
                     <div class="font-bold text-xl mb-2">Mohamed Daboub</div>
                     <p class="text-white text-base">
-                    Co-créateur de let's draw, Développeur.
+                    Co-Créateur de let's draw, Développeur.
                     </p>
                 </div>
                 <div class="px-6 pt-4 pb-2 flex justify-center gap-7">
                     <span class="inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2 mb-2  hover:animate-[spin_2000ms_1]  "><a href="https://discord.com/channels/@me"><i class="bi bi-discord"></i></a></span>
                     <span class="inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2 mb-2  hover:animate-[spin_2000ms_1] "><a href="https://www.instagram.com/daboub_mohamed_/"><i class="bi bi-instagram"></i></a></span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2 mb-2  hover:animate-[spin_2000ms_1] "> <a href="https://www.linkedin.com/in/mohamed-daboub-12376a224/"><i class="bi bi-linkedin "></i></a></span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2 mb-2  hover:animate-[spin_2000ms_1] "> <a href="https://www.linkedin.com/in/mohamed-daboub/"><i class="bi bi-linkedin "></i></a></span>
+                </div>
+            </div>
+            <div class="max-w-sm rounded overflow-hidden shadow-lg bg-gradient-to-t from-batman to-batman1 ">
+                <img class="w-full" src="../../public/img-squirrel/Ludovic_Faye.webp" alt="une image de Mohamed Daboub qui fait le Développement dans le groupe">
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">Ludovic Faye</div>
+                    <p class="text-white text-base">
+                    Co-Créateur de let's draw, Communication.
+                    </p>
+                </div>
+                <div class="px-6 pt-4 pb-2 flex justify-center gap-7">
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2 mb-2  hover:animate-[spin_2000ms_1] "><a href="https://www.facebook.com/profile.php?id=100004857558295"><i class="bi bi-facebook"></i></a></span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2 mb-2  hover:animate-[spin_2000ms_1] "> <a href="https://www.linkedin.com/in/ludovic-faye-7b0836186/"><i class="bi bi-linkedin "></i></a></span>
                 </div>
             </div>
             
--- src/views/GlossaireView.vue
+++ src/views/GlossaireView.vue
@@ -1,20 +1,20 @@
 <template>
-    <h1 class=" text-white mx-24 text-7xl pt-12 font-medium">Glossaire</h1>
+    <h1 class=" text-white mx-24 text-4xl pt-12 font-medium">Glossaire</h1>
     <dl class=" text-white mx-10 py-10 pb-24">
-        <dt class=" py-8 text-3xl">Hyperréalisme</dt>
-        <dd class=" text-lg ml-10">Courant artistique (peinture, sculpture) né aux États-Unis à la fin des années 1960, caractérisé par un rendu minutieux de la réalité inspiré d'images photographiques.</dd>
-        <dt class=" py-8 text-3xl">Dessin au trait</dt>
-        <dd class=" text-lg ml-10">Un dessin au trait est simplement tracé pour ne représenter que les contours des objets. Ces lignes sont accusées à l'encre ou au crayon.</dd>
-        <dt class=" py-8 text-3xl">Le Gribouillage</dt>
-        <dd class=" text-lg ml-10">Types de dessin qui laisse subconscient circuler. C’est une façon de joué avec les ombres et les formes qu’ils trouvent au quotidien et les transforment en œuvres d’art fantaisistes avec leurs griffonnages. Le griffonnage est une impression merveilleuse et immédiate du monde devant nous. Ils sont censés être exécutés rapidement.</dd>
-        <dt class=" py-8 text-3xl">L’Anamorphique</dt>
-        <dd class=" text-lg ml-10">Style de dessin assez technique qui consiste à représenté un dessin en 3D. Les 3D requièrent une très bonne maîtrise de la perspective pour créer profondeur et illusion. Bien que cela prenne un peu de temps et de pratique pour se familiariser, les résultats sont des œuvres d’art étonnantes et surprenantes.</dd>
-        <dt class=" py-8 text-3xl">L’Architectural</dt>
-        <dd class=" text-lg ml-10">Style de dessin assez technique qui est souvent utilisé dans le monde du design pour représenter des batiments. Ce type de dessin nécessite beaucoup de précision et d’attention aux détails. Et cela s’applique soit pour dessiner une réplique exacte d’un bâtiment historique, soit pour créer sa propre pièce d’architecture fantastique.</dd>
-        <dt class=" py-8 text-3xl">Le Pointillisme</dt>
-        <dd class=" text-lg ml-10">Style de dessin qui consiste à dessiner entièrement avec des points. Les artistes peuvent créer de l’ombre et de la lumière en pointillant des centaines, voire des milliers, de minuscules points. Très souvent, le réalisme des résultats peut être choquants.</dd>
-        <dt class=" py-8 text-3xl">Le Dessin de mode</dt>
-        <dd class=" text-lg ml-10" >Les illustrations de mode sont souvent utilisées par les créateurs pour mettre leurs idées sur papier. Il s’agit de dessin fait rapidement qui transmet l’essence d’un vêtement. Pour ce style, l’accent est mis sur les vêtements et non sur le visage. Les traits du visage sont souvent inexistants ou minimes.</dd>
+        <dt class=" py-8 text-2xl">Hyperréalisme</dt>
+        <dd class=" text-lg mx-10">Courant artistique (peinture, sculpture) né aux États-Unis à la fin des années 1960, caractérisé par un rendu minutieux de la réalité inspiré d'images photographiques.</dd>
+        <dt class=" py-8 text-2xl">Dessin au trait</dt>
+        <dd class=" text-lg mx-10 sm:mx-0">Un dessin au trait est simplement tracé pour ne représenter que les contours des objets. Ces lignes sont accusées à l'encre ou au crayon.</dd>
+        <dt class=" py-8 text-2xl">Le Gribouillage</dt>
+        <dd class=" text-lg mx-10">Types de dessin qui laisse subconscient circuler. C’est une façon de joué avec les ombres et les formes qu’ils trouvent au quotidien et les transforment en œuvres d’art fantaisistes avec leurs griffonnages. Le griffonnage est une impression merveilleuse et immédiate du monde devant nous. Ils sont censés être exécutés rapidement.</dd>
+        <dt class=" py-8 text-2xl">L’Anamorphique</dt>
+        <dd class=" text-lg mx-10">Style de dessin assez technique qui consiste à représenté un dessin en 3D. Les 3D requièrent une très bonne maîtrise de la perspective pour créer profondeur et illusion. Bien que cela prenne un peu de temps et de pratique pour se familiariser, les résultats sont des œuvres d’art étonnantes et surprenantes.</dd>
+        <dt class=" py-8 text-2xl">L’Architectural</dt>
+        <dd class=" text-lg mx-10">Style de dessin assez technique qui est souvent utilisé dans le monde du design pour représenter des batiments. Ce type de dessin nécessite beaucoup de précision et d’attention aux détails. Et cela s’applique soit pour dessiner une réplique exacte d’un bâtiment historique, soit pour créer sa propre pièce d’architecture fantastique.</dd>
+        <dt class=" py-8 text-2xl">Le Pointillisme</dt>
+        <dd class=" text-lg mx-10">Style de dessin qui consiste à dessiner entièrement avec des points. Les artistes peuvent créer de l’ombre et de la lumière en pointillant des centaines, voire des milliers, de minuscules points. Très souvent, le réalisme des résultats peut être choquants.</dd>
+        <dt class=" py-8 text-2xl">Le Dessin de mode</dt>
+        <dd class=" text-lg mx-10" >Les illustrations de mode sont souvent utilisées par les créateurs pour mettre leurs idées sur papier. Il s’agit de dessin fait rapidement qui transmet l’essence d’un vêtement. Pour ce style, l’accent est mis sur les vêtements et non sur le visage. Les traits du visage sont souvent inexistants ou minimes.</dd>
     </dl>
 
 </template>
