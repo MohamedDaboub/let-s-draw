59418955f79bb961ee7966558d010db5060afd21
Flo714 <florentin.demortiere@edu.univ-fcomte.fr>
2022-05-24 21:30:03 +0200

    Page profil presque fini (manque 2 img + 1 btn)

---
+++ src/components/BoutonView.vue
@@ -0,0 +1,12 @@
+<template>
+    <button class="bg-bleufonce px-6 py-2 rounded-3xl absolute right-24">
+        <h3 class="text-white">{{Nom}}</h3>
+    </button>
+</template>
+<script>
+export default {
+    props: {
+        Nom: String
+    }
+}
+</script>
--- src/views/ProfilView.vue
+++ src/views/ProfilView.vue
@@ -5,7 +5,7 @@
             <img class="rounded-full border-4 border-white ml-24 w-52" src="../../public/img-squirrel/image-35.webp"
                 alt="">
         </div>
-        <div class="text-white grid grid-col-2">
+        <div class="text-white flex flex-col">
             <div class="flex gap-52">
                 <div>
                     <p>Votre nom</p>
@@ -24,45 +24,50 @@
             </div>
             <div>
                 <p>Votre email</p>
-                <div class="flex gap-36 my-4 ml-4 bg-bleutransparent2 p-2 rounded-xl text-black w-84">
+                <div class="flex gap-[139px] my-4 ml-4 bg-bleutransparent2 p-2 rounded-xl text-black">
                     <p>antoine.garot@gmail.com</p>
                     <Modifier class="" />
                 </div>
             </div>
             <div>
                 <p>Votre mot de passe</p>
-                <div class="flex gap-36 my-4 ml-4 bg-bleutransparent2 p-2 rounded-xl text-black">
+                <div class="flex gap-[280px] my-4 ml-4 bg-bleutransparent2 p-2 rounded-xl text-black">
                     <p>............</p>
                     <Modifier class="" />
                 </div>
             </div>
         </div>
     </div>
-    <div>
-        <h2>Biographie</h2>
-        <p>Je suis actuellement à la recherche d’un artiste spécialiste du pointillisme. J’aimerai qu’on reprenne mon
+    <div class="my-6 mx-10">
+        <h2 class="text-4xl text-white">Biographie</h2>
+        <div class="flex justify-between bg-bleutransparent2 p-3 rounded-xl mt-4 mr-40">
+            <p class="">Je suis actuellement à la recherche d’un artiste spécialiste du pointillisme. J’aimerai qu’on reprenne mon
             dessin d’enfance.</p>
-    </div>
-    <div>
-        <h2>Notifications</h2>
-        <div>
-            <p>1</p>
-            <p>Vous avez reçu un nouveau dessin</p>
+            <Modifier class="" />
         </div>
-        <p>Florent Vacot a terminer votre dessin, il y a 2 jours. Vous pouvez dès à présent consulter votre dessin en
-            cliquant sur ce bouton ci-dessous</p>
-        <!-- Composant "Style dessin" -->
     </div>
-    <div>
-        <h2>Faîtes un don</h2>
+    <div class="my-10 mx-10">
+        <h2 class="text-4xl text-white">Notifications</h2>
+        <div class="text-2xl flex mt-8 ml-20 text-white">
+            <p class="bg-red-700 rounded-full p-1">1</p>
+            <p class="mx-4 p-1">Vous avez reçu un nouveau dessin</p>
+        </div>
+        <p class="bg-bleutransparent2 p-3 rounded-xl mr-40 mt-6">Florent Vacot a terminer votre dessin, il y a 2 jours. Vous pouvez dès à présent consulter votre dessin en
+            cliquant sur ce bouton ci-dessous</p>
+
+    </div>
+    <bouton  Nom="Consulter mon dessin" />
+    <div class="mx-10">
+        <h2 class="text-4xl text-white mt-20">Faîtes un don</h2>
         <div>
-            <h3>L'objectif du don</h3>
-            <p>Le don a pour but d’aider les artistes dans leur carrière ou futur carrière, de soutenir l’association
+            <h3 class="text-2xl text-white my-6 mx-6">L'objectif du don</h3>
+            <p class="w-1/2 ml-16">Le don a pour but d’aider les artistes dans leur carrière ou futur carrière, de soutenir l’association
                 “unicef” qui lutte pour les enfants défavorisés et de nous aider à continuer de faire fonctionner Let’s
                 Draw.</p>
+            <img src="" alt="">
         </div>
         <div>
-            <h3>Répartition du don</h3>
+            <h3 class="text-2xl text-white my-6 mx-6">Répartition du don</h3>
             <img src="" alt="">
         </div>
     </div>
@@ -70,9 +75,10 @@
 </template>
 <script>
 import Modifier from "../components/icons/ModifierView.vue"
+import Bouton from "../components/BoutonView.vue"
 
 export default {
     name: "App",
-    components: { Modifier },
+    components: { Modifier, Bouton },
 };
 </script>
