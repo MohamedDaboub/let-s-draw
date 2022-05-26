c24859afd64802350036460594a523a0e6e4a6fd
LudoLaFayette <ludovicfaye89@gmail.com>
2022-05-26 15:41:03 +0200

    init Firebase

--- src/main.js
+++ src/main.js
@@ -2,6 +2,23 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import router from './router'
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBVXupGxh3t_GF-FJqEq7pTcIM1OS6Ds1M",
+    authDomain: "projet-bdd-letsdraw.firebaseapp.com",
+    projectId: "projet-bdd-letsdraw",
+    storageBucket: "projet-bdd-letsdraw.appspot.com",
+    messagingSenderId: "16362354414",
+    appId: "1:16362354414:web:21ea24c04bc90ff8dae5ff"
+  };
+
+  // Initialize Firebase
+  const appFirebase = initializeApp(firebaseConfig);
 
 const app = createApp(App)
 app.use(router)
