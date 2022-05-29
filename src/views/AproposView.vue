9278d89bc2f6a61c7e7021e9d276fd9af9456321
Flo714 <florentin.demortiere@edu.univ-fcomte.fr>
2022-05-29 18:33:55 +0200

    Correction page A propos

--- src/views/AproposView.vue
+++ src/views/AproposView.vue
@@ -4,7 +4,7 @@
         <p class=" text-xl px-16 py-8 ">Nous sommes 3 étudiants en BUT MMI. Nous avons réaliser ce concept lors d’un projet durant notre 1 ère année.  Merci de votre compréhension.</p>
         <div class="flex gap-10 justify-center py-10 flex-wrap">
             <div class="max-w-sm rounded overflow-hidden shadow-lg bg-gradient-to-t from-batman to-batman1 ">
-                <img class="w-full" src="../../public/img-squirrel/Florenten_Demortiere.webp" alt="une image de Mohamed Daboub qui fait le Développement dans le groupe">
+                <img class="w-full" src="../../public/img-squirrel/Florenten_Demortiere.webp" alt="une image de Florentin Demortiere qui fait le Design dans le groupe">
                 <div class="px-6 py-4">
                     <div class="font-bold text-xl mb-2">Florentin Demortiere</div>
                     <p class="text-white text-base">
@@ -12,9 +12,9 @@
                     </p>
                 </div>
                 <div class="px-6 pt-4 pb-2 flex justify-center gap-7">
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2 mb-2  hover:animate-[spin_2000ms_1]  "><a href="https://www.pinterest.fr/flodemortiere9154/_saved/"><i class="bi bi-pinterest"></i></a></span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2 mb-2  hover:animate-[spin_2000ms_1] "><a href="https://www.instagram.com/flo.dem_/"><i class="bi bi-instagram"></i></a></span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2 mb-2  hover:animate-[spin_2000ms_1] "> <a href="https://www.linkedin.com/in/florentin-demortiere-a19551224/"><i class="bi bi-linkedin "></i></a></span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2 mb-2  hover:animate-[spin_1000ms_1]  "><a href="https://www.pinterest.fr/flodemortiere9154/_saved/"><i class="bi bi-pinterest"></i></a></span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2 mb-2  hover:animate-[spin_1000ms_1] "><a href="https://www.instagram.com/flo.dem_/"><i class="bi bi-instagram"></i></a></span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2 mb-2  hover:animate-[spin_1000ms_1] "> <a href="https://www.linkedin.com/in/florentin-demortiere-a19551224/"><i class="bi bi-linkedin "></i></a></span>
                 </div>
             </div>
             <div class="max-w-sm rounded overflow-hidden shadow-lg bg-gradient-to-t from-batman to-batman1 ">
@@ -26,13 +26,13 @@
                     </p>
                 </div>
                 <div class="px-6 pt-4 pb-2 flex justify-center gap-7">
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2 mb-2  hover:animate-[spin_2000ms_1]  "><a href="https://discord.com/channels/@me"><i class="bi bi-discord"></i></a></span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2 mb-2  hover:animate-[spin_2000ms_1] "><a href="https://www.instagram.com/daboub_mohamed_/"><i class="bi bi-instagram"></i></a></span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2 mb-2  hover:animate-[spin_2000ms_1] "> <a href="https://www.linkedin.com/in/mohamed-daboub/"><i class="bi bi-linkedin "></i></a></span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2 mb-2  hover:animate-[spin_1000ms_1]  "><a href="https://discord.com/channels/@me"><i class="bi bi-discord"></i></a></span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2 mb-2  hover:animate-[spin_1000ms_1] "><a href="https://www.instagram.com/daboub_mohamed_/"><i class="bi bi-instagram"></i></a></span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2 mb-2  hover:animate-[spin_1000ms_1] "> <a href="https://www.linkedin.com/in/mohamed-daboub/"><i class="bi bi-linkedin "></i></a></span>
                 </div>
             </div>
             <div class="max-w-sm rounded overflow-hidden shadow-lg bg-gradient-to-t from-batman to-batman1 ">
-                <img class="w-full" src="../../public/img-squirrel/Ludovic_Faye.webp" alt="une image de Mohamed Daboub qui fait le Développement dans le groupe">
+                <img class="w-full" src="../../public/img-squirrel/Ludovic_Faye.webp" alt="une image de Ludovic Faye qui fait la Communication dans le groupe">
                 <div class="px-6 py-4">
                     <div class="font-bold text-xl mb-2">Ludovic Faye</div>
                     <p class="text-white text-base">
@@ -40,8 +40,8 @@
                     </p>
                 </div>
                 <div class="px-6 pt-4 pb-2 flex justify-center gap-7">
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2 mb-2  hover:animate-[spin_2000ms_1] "><a href="https://www.facebook.com/profile.php?id=100004857558295"><i class="bi bi-facebook"></i></a></span>
-                    <span class="inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2 mb-2  hover:animate-[spin_2000ms_1] "> <a href="https://www.linkedin.com/in/ludovic-faye-7b0836186/"><i class="bi bi-linkedin "></i></a></span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2 mb-2  hover:animate-[spin_1000ms_1] "><a href="https://www.facebook.com/profile.php?id=100004857558295"><i class="bi bi-facebook"></i></a></span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-2 text-sm font-semibold text-gray-700 mr-2 mb-2  hover:animate-[spin_1000ms_1] "> <a href="https://www.linkedin.com/in/ludovic-faye-7b0836186/"><i class="bi bi-linkedin "></i></a></span>
                 </div>
             </div>
             
