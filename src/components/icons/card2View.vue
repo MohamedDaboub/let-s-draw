189c387ea2ed25d2479be5a2e92134f15ee12add
LudoLaFayette <ludovicfaye89@gmail.com>
2022-06-08 16:23:14 +0200

    insertion cards synchro artiste firebase

---
+++ src/components/icons/card2View.vue
@@ -0,0 +1,80 @@
+<template>
+<div class="mx-5 grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(345px,1fr))] gap-8 p-10 py-20">
+    <div class="p-6 flex bg-bleufonce rounded-xl" v-for="artiste in listeArtiste" :key="artiste.id">
+            <img class="rounded-full w-32 h-32" :src="artiste.photo" :alt="artiste.description">
+            <div class="flex flex-col text-white ml-4">
+                <p class="font-bold text-2xl mt-6"> {{ artiste.prenom }} {{ artiste.nom }}</p>
+                <p class="text-lg ml-4 mt-1">{{ artiste.role }}</p>
+            </div>
+
+    </div>
+</div>
+</template>
+<script>
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDocs,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  onSnapshot,
+  query,
+  orderBy,
+} from "https://www.gstatic.com/firebasejs/9.7.0/firebase-firestore.js";
+import {
+  getStorage,
+  ref, 
+  getDownloadURL,
+  uploadString,
+} from "https://www.gstatic.com/firebasejs/9.7.0/firebase-storage.js";
+export default {
+
+
+   name: "cardV2view",
+  data() {
+    return {
+      listeArtiste: [],
+    };
+  },
+  mounted() {
+    this.getArtiste();
+  },
+  methods: {
+    async getArtiste() {
+      // Obtenir Firestore
+      const firestore = getFirestore();
+      // Base de données(collection)document participant
+      const dbArtiste = collection(firestore, "artiste");
+      // Liste des participants triés sur leur nom
+      const q = query(dbArtiste, orderBy("nom", "asc"));
+      await onSnapshot(q, (snapshot) => {
+        this.listeArtiste = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        // Récupération des images de chaque participant
+        // parcours de la liste
+
+        this.listeArtiste.forEach(function (artiste) {
+          // Obtenir le Cloud Storage
+          const storage = getStorage();
+          // Récupération de l'image par son nom de fichier
+          const spaceRef = ref(storage, "artiste/" + artiste.photo);
+          // Récupération de l'url complète de l'image
+          getDownloadURL(spaceRef)
+            .then((url) => {
+              // On remplace le nom du fichier
+              // Par l'url complète de la photo
+              artiste.photo = url;
+            })
+            .catch((error) => {
+              console.log("erreur downloadUrl", error);
+            });
+        });
+      });
+    },
+  },
+}
+</script>
--- src/views/choixArtisteView.vue
+++ src/views/choixArtisteView.vue
@@ -1,59 +1,52 @@
 <template>
-<!-- liste java d'artistes ? -->
-    <body>
-        <section class="p-10">
-            <h1 class="text-white text-3xl">Choix de l’artiste</h1>
+  <!-- liste java d'artistes ? -->
+  <body>
+    <section class="p-10">
+      <h1 class="text-3xl text-white">Choix de l’artiste</h1>
 
-            <p class="text-white text-lg pt-10">Pour envoyer votre dessin veullez choisir un artiste en fonction de son style et de vos préférences</p>
-        </section>
+      <p class="pt-10 text-lg text-white">
+        Pour envoyer votre dessin veullez choisir un artiste en fonction de son style et de vos préférences
+      </p>
+    </section>
 
-        <div class="ml-10 text-white md:flex gap-4 bg-bleutransparent2 rounded-3xl p-2 border-2 border-white md:w-1/3 grid grid-cols-2 w-2/3 ">
-            <p>Trier par :</p>
+    <div class="ml-10 grid w-2/3 grid-cols-2 gap-4 rounded-3xl border-2 border-white bg-bleutransparent2 p-2 text-white md:flex md:w-1/3">
+      <p>Trier par :</p>
 
-            <div class="flex gap-2 border-b-2">
-                <p>Tous</p>
-                <flechebas class="mt-2" />
-            </div>
+      <div class="flex gap-2 border-b-2">
+        <p>Tous</p>
+        <flechebas class="mt-2" />
+      </div>
 
-            <div class="flex gap-2 border-b-2">
-                <p>Les mieux notés</p>
-                <flechebas class="mt-2" />
-            </div>
+      <div class="flex gap-2 border-b-2">
+        <p>Les mieux notés</p>
+        <flechebas class="mt-2" />
+      </div>
 
-            <div class="flex gap-2 border-b-2">
-                <p>De la semaine</p>
-                <flechebas class="mt-2" />
-            </div>
-        </div>
-
-        <div class="p-10 py-20 grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(345px,1fr))] gap-8 mx-5">
-            <RouterLink to="/profilArtiste"><Card1 image="/public/img-squirrel/Florent_Vacot.webp"
-                    nom="Florent Vacot"
-                    role="Peintre"/></RouterLink>
-            <RouterLink to="/profilArtiste"><Card1 image="/public/img-squirrel/image_1.webp"
-                    nom="Manon Bernard"
-                    role="Peintre"/></RouterLink>
-            <RouterLink to="/profilArtiste"><Card1 image="/public/img-squirrel/image_5.webp"
-                    nom="Pauline Fort"
-                    role="Peintre"/></RouterLink>
-            <RouterLink to="/profilArtiste"><Card1 image="/public/img-squirrel/image12.webp"
-                    nom="Lance Leblanc"
-                    role="Peintre"/></RouterLink>
-            <RouterLink to="/profilArtiste"><Card1 image="/public/img-squirrel/image13.webp"
-                    nom="Florent Vacot"
-                    role="Peintre"/></RouterLink>
-        </div>
-        
-
-    </body>
-
+      <div class="flex gap-2 border-b-2">
+        <p>De la semaine</p>
+        <flechebas class="mt-2" />
+      </div>
+    </div>
+<RouterLink to="/profilArtiste"><Card2 /></RouterLink>
+    <!-- <div class="mx-5 grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(345px,1fr))] gap-8 p-10 py-20"> -->
+      <!-- <RouterLink to="/profilArtiste"
+        ><Card1 image="/public/img-squirrel/Florent_Vacot.webp" nom="Florent Vacot" role="Peintre"
+      /></RouterLink>
+      <RouterLink to="/profilArtiste"><Card1 image="/public/img-squirrel/image_1.webp" nom="Manon Bernard" role="Peintre" /></RouterLink>
+      <RouterLink to="/profilArtiste"><Card1 image="/public/img-squirrel/image_5.webp" nom="Pauline Fort" role="Peintre" /></RouterLink>
+      <RouterLink to="/profilArtiste"><Card1 image="/public/img-squirrel/image12.webp" nom="Lance Leblanc" role="Peintre" /></RouterLink>
+      <RouterLink to="/profilArtiste"><Card1 image="/public/img-squirrel/image13.webp" nom="Florent Vacot" role="Peintre" /></RouterLink> -->
+      
+    <!-- </div> -->
+  </body>
 </template>
 <script>
-import flechebas from "../components/icons/flechebasView.vue"
-import Card1 from "../components/Card1View.vue"
+import flechebas from "../components/icons/flechebasView.vue";
+import Card1 from "../components/Card1View.vue";
+import Card2 from "../components/icons/card2View.vue";
 
 export default {
   name: "App",
-  components: { flechebas, Card1 },
+  components: { flechebas, Card1, Card2 },
 };
 </script>
