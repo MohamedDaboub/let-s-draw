1a701241518870cfe6318b3813500828dbb36b83
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-06-09 14:00:35 +0200

    card 3 fait

---
+++ src/components/Card3View.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex bg-noir rounded-xl flex-col">
+            <img class="rounded-t-lg w-full h-full" :src="image" alt="Image de l'artiste">
+            <div class="flex text-white mx-4">
+                <p class="font-bold text-2xl my-6">{{ nom }}</p>
+            </div>
+
+    </div>
+</template>
+<script>
+
+export default {
+    props :{
+        image: String,
+        nom: String,
+    },
+}
+</script>
