d1a1a7233a1897348e791fb3fe814f124657f568
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-06-09 14:05:02 +0200

    modif typo dans la card

--- src/components/Card3View.vue
+++ src/components/Card3View.vue
@@ -2,7 +2,7 @@
     <div class="flex bg-noir rounded-xl flex-col">
             <img class="rounded-t-lg w-full h-full" :src="image" alt="Image de l'artiste">
             <div class="flex text-white mx-4">
-                <p class="font-bold text-2xl my-6">{{ nom }}</p>
+                <h2 class="font-bold font-chivo text-2xl my-6">{{ nom }}</h2>
             </div>
 
     </div>
